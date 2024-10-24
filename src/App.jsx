565ef28b89d4793288f5c44a43cb7a63dd7c550d
witphatz <azen555666@gmail.com>
2024-10-24 11:26:36 +0700

    axios.get

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_API_URL}/users`)
+    axios.get(`http://192.168.56.1:5000/users`)
       .then(response => {
         setUsers(response.data);
       })
